9194a75962b56b77f5c2ab14b4528ad1f1f97aa1
ShunIshizeki <shun2000c9@outlook.jp>
2023-05-26 17:00:00 +0900

    feat: 画像を削除

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const commentRoute = require("./routes/comments");
-const path = require("path");
 const mongoose = require("mongoose");
 // envファイル変更後はサーバーを再起動するのを忘れない
 require("dotenv").config();
@@ -36,7 +35,6 @@ const corsOpts = {
   exposedHeaders: ["Content-Type"],
 };
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(cors(corsOpts));
 app.use(express.json());
 app.use("/api/users", userRoute);
